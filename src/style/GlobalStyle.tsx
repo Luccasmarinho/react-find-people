8dc004fb38bcae5d3fbca74a92d2aec987ae41f4
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-10-18 19:19:08 -0300

    adding global style

---
+++ src/style/GlobalStyle.tsx
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        outline: none;
+    }
+
+    :root {
+        --background: #171717;
+        --text: #f1f1f1;
+        --primary: #811ac0;
+        --grey: #414141;
+        --grey2: #717171;
+    }
+
+    body {
+        min-height: 100vh;
+        background-color: var(--background);
+    }
+`;
+
+export default GlobalStyle;
