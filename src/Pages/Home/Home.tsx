21abf67239b6ea7a535dd038afc0dd537d70503d
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-10-18 23:42:14 -0300

    Create folder Home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,6 @@
-import BasicPagination from "../Components/BasicPagination/BasicPagination";
-import InputSearch from "../Components/InputSearch/InputSearch";
-import Teste from "../Components/Table/Table";
+import BasicPagination from "../../Components/BasicPagination/BasicPagination";
+import InputSearch from "../../Components/InputSearch/InputSearch";
+import Teste from "../../Components/Table/Table";
 import { Container } from "./HomeStyle";
 
 const Home = () => {
--- src/Router/RouterApp.tsx
+++ src/Router/RouterApp.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "../Pages/Home";
+import Home from "../Pages/Home/Home";
 
 const RoutesApp = () => {
   return (
