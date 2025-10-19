5bb2af02221302f3a3670883a9d1158120c384f7
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-10-18 22:00:13 -0300

    Create home page

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,16 @@
+import BasicPagination from "../Components/BasicPagination/BasicPagination";
+import InputSearch from "../Components/InputSearch/InputSearch";
+import Teste from "../Components/Table/Table";
+import { Container } from "./HomeStyle";
+
+const Home = () => {
+  return (
+    <Container>
+      <InputSearch />
+      <Teste />
+      <BasicPagination />
+    </Container>
+  );
+};
+
+export default Home;
