612697964bccc409d6a1bc0fa6131b120933c6d7
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-10-20 18:34:16 -0300

    Wrapped context config

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import GlobalStyle from "./style/GlobalStyle.tsx";
 import RoutesApp from "./Router/RouterApp.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ProviderContext from "./context/ProviderContext.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RoutesApp />
-      <GlobalStyle />
-    </QueryClientProvider>
+    <ProviderContext>
+      <QueryClientProvider client={queryClient}>
+        <RoutesApp />
+        <GlobalStyle />
+      </QueryClientProvider>
+    </ProviderContext>
   </StrictMode>
 );
