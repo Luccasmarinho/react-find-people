48c788defe80cea1126cdd5d79e0b9f313993f09
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-10-19 22:07:04 -0300

    Config use query

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import GlobalStyle from "./style/GlobalStyle.tsx";
 import RoutesApp from "./Router/RouterApp.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <RoutesApp />
       <GlobalStyle />
+    </QueryClientProvider>
   </StrictMode>
 );
