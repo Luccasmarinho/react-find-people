493d1dc5939eba87fc00105f52ac508d893b6095
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-10-20 16:10:59 -0300

    adding Loading component

---
+++ src/Loading/Loading.tsx
@@ -0,0 +1,18 @@
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+export default function CircularIndeterminate() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        height: "200px",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <CircularProgress sx={{ color: "#811ac0" }} />
+    </Box>
+  );
+}
